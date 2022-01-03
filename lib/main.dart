ccf4de79cb78603a3285277d44db6816652bc33e
Bence Rozsa <robence@gmail.com>
2022-01-03 17:43:09 +0100

    style business card

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,74 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.red,
-                width: 100.0,
-              ),
-              Column(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green,
+                children: const [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('images/Portrait_2.jpg'),
                   ),
                 ],
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
+              const Text(
+                'Bence Rozsa',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              const Text(
+                'Software Engineer',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 20,
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              Card(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+36 123 456 789',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'example@domain.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
